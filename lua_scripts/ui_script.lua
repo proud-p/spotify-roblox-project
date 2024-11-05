5d690119449d0da064de88c92fa7a2f1149aa769
Ben Shapley <benjamin.shapley@hogarth.com>
2024-11-05 09:58:35 +0000

    inital commit with functionality

---
+++ lua_scripts/ui_script.lua
@@ -0,0 +1,16 @@
+-- INSERT PARAMETERS
+
+local player = game.Players.LocalPlayer 
+local on_screen = player:WaitForChild("PlayerGui"):WaitForChild("ScreenGui"):WaitForChild("Frame"):WaitForChild("Text")
+
+local function updateDisplay()
+	while true do
+		for i, item in ipairs(data) do
+			on_screen.Text = "Song: '" .. item.song .. "' by: " .. item.artist
+			print(i)
+			wait(3)
+		end
+	end
+end
+
+coroutine.wrap(updateDisplay)()
